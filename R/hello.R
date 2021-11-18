e519c35317afd7cafe3975990b354a7883f32e4e
kaelimonahan1 <90069771+kaelimonahan1@users.noreply.github.com>
2021-11-18 12:03:17 -0700

    fixed error

--- R/hello.R
+++ R/hello.R
@@ -116,7 +116,7 @@ ground_beef_avail <- function(lean_beef_varieties) {
 
 #' Compute the availability points associated with hot dogs
 #'
-#' This function takes in the number of varieties of fat free hot and light hot dogs and returns the NEMS-S points associated with availability.
+#' This function takes in the number of varieties of fat free and light hot dogs and returns the NEMS-S points associated with availability.
 #'
 #' @details This function implements the scoring method described in Table X of the NEMS-S manual. "Fat free hot dogs" have 0 grams of fat per serving, and "light hot dogs" have less than or equal to 7 grams of fat per serving.
 #' @param fat_free_hot_dogs The number of varieties of fat free hot dogs (0g fat per serving).
