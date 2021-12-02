922412673fa6e62c87e64c39ae5a628abef6b470
kaelimonahan1 <90069771+kaelimonahan1@users.noreply.github.com>
2021-12-02 12:45:56 -0700

    added a read_nemss function

--- R/nems_s.R
+++ R/nems_s.R
@@ -1,22 +1,14 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
 
+# This is my function.
 
 convert_files <- function() {
   #convert files given into csv
 }
+
+read_nemss <- function(file) {
+  read_csv(file)
+}
+
 
 # Availability (30 points)
 #'Compute milk availability points
