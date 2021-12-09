5b7608551df9a4184acfe84dfd7f75f29e2634d5
kaelimonahan1 <90069771+kaelimonahan1@users.noreply.github.com>
2021-12-09 12:15:40 -0700

    finish this code

--- R/nemsr.r
+++ R/nemsr.r
@@ -10,6 +10,9 @@
 #' @section Documentation and References:
 #' All the information for the scoring system and classification of "healthier" options come from the NEMS-S Protocol Measures and the Lundsford 2016 Thesis (most information comes from Table 4.1).
 #'
+#'@section Objective:
+#' at byu we want to ... where we get the info ... how to use each package... notes on functions (put this information into the read me file as well)
+#'
 #' @docType package
 #' @name nemsr
 NULL
