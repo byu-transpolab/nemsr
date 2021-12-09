d38105f236088abba3204d6390627f524062afc7
kaelimonahan1 <90069771+kaelimonahan1@users.noreply.github.com>
2021-12-09 11:40:43 -0700

    adding details

--- R/nemsr.r
+++ R/nemsr.r
@@ -1,10 +1,16 @@
 #' nemsr: A package for working with NEMS-S survey data
 #'
+#'
 #' The foo package provides three categories of important functions:
 #' foo, bar and baz.
 #'
 #' @section Foo functions:
 #' The foo functions ...
+#'
+#' @section Documentation and References
+#' NEMS-S Protocol
+#' NEMS-S
+#' Lundsford (2016) Thesis (most information comes from Table 4.1)
 #'
 #' @docType package
 #' @name nemsr
